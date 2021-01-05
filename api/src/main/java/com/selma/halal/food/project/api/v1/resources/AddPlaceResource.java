37613ce6b90dec3a0ebb1cc9d8d795bebeed65de
selmaHalilcevic <selmahalilcevicius@gmail.com>
2021-01-05 21:12:19 +0100

    adding api implementation

---
+++ api/src/main/java/com/selma/halal/food/project/api/v1/resources/AddPlaceResource.java
@@ -0,0 +1,92 @@
+package com.selma.halal.food.project.api.v1.resources;
+
+import com.selma.halal.food.project.lib.AddPlaceMetadata;
+import com.selma.halal.food.project.services.beans.AddPlaceBean;
+
+import javax.enterprise.context.RequestScoped;
+import javax.inject.Inject;
+import javax.ws.rs.*;
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
+import javax.ws.rs.core.UriInfo;
+import java.util.List;
+import java.util.logging.Logger;
+
+@RequestScoped
+@Consumes(MediaType.APPLICATION_JSON)
+@Produces(MediaType.APPLICATION_JSON)
+@Path("/addplace")
+public class AddPlaceResource {
+
+    private Logger log = Logger.getLogger(AddPlaceResource.class.getName());
+
+    @Inject
+    private AddPlaceBean addPlaceBean;
+
+    @Context
+    protected UriInfo uriInfo;
+
+    @GET
+    public Response getAddPlaceMetadata() {
+        List<AddPlaceMetadata> addPlaceMetadata = addPlaceBean.getAddPlaceMetadataFilter(uriInfo);
+
+        return Response.status(Response.Status.OK).entity(addPlaceMetadata).build();
+
+    }
+
+    @GET
+    @Path("/{id}")
+    public Response getAddPlaceMetadata(@PathParam("id") Integer id) {
+
+        AddPlaceMetadata addPlaceMetadata = addPlaceBean.getAddPlaceMetadata(id);
+
+        if (addPlaceMetadata == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        return Response.status(Response.Status.OK).entity(addPlaceMetadata).build();
+    }
+
+    @POST
+    public Response createAddPlaceMetadata(AddPlaceMetadata addPlaceMetadata) {
+
+        if ((addPlaceMetadata.getName() == null || addPlaceMetadata.getCity() == null || addPlaceMetadata.getCountry() == null)) {
+            return Response.status(Response.Status.BAD_REQUEST).build();
+        }
+        else {
+            addPlaceMetadata = addPlaceBean.createAddPlaceMetadata(addPlaceMetadata);
+        }
+
+        return Response.status(Response.Status.OK).entity(addPlaceMetadata).build();
+
+    }
+
+    @PUT
+    @Path("/{id}")
+    public Response putHalalPlaceMetadata(@PathParam("id") Integer id,
+                                          AddPlaceMetadata addPlaceMetadata) {
+
+        addPlaceMetadata = addPlaceBean.putAddPlaceMetadata(id, addPlaceMetadata);
+
+        if (addPlaceMetadata == null) {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+
+        return Response.status(Response.Status.NOT_MODIFIED).build();
+    }
+
+    @DELETE
+    @Path("/{id}")
+    public Response deleteHalalPlaceMetadata(@PathParam("id") Integer id) {
+
+        boolean deleted = addPlaceBean.deleteAddPlaceMetadata(id);
+
+        if (deleted) {
+            return Response.status(Response.Status.NO_CONTENT).build();
+        }
+        else {
+            return Response.status(Response.Status.NOT_FOUND).build();
+        }
+    }
+}
