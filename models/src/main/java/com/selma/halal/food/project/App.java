96abc30425652114a40b18d599472c112bd3c364
selmaHalilcevic <selmahalilcevicius@gmail.com>
2021-01-05 16:34:13 +0100

    adding microservice modules with dependencies

---
+++ models/src/main/java/com/selma/halal/food/project/App.java
@@ -0,0 +1,13 @@
+package com.selma.halal.food.project;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
